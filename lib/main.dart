08b56d2bba1ce8d402fad921a9286f9af607cf4b
Wildor Gutierrez <wgutierrezsanchez@gmail.com>
2022-06-23 20:49:34 -0500

    part3

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  int activeIndex = 0;
+  final urlImages = [
+    'https://static01.nyt.com/images/2018/02/11/us/11dc-pelosi/11dc-pelosi-jumbo.jpg?quality=75&auto=webp',
+    'https://static01.nyt.com/images/2017/06/26/opinion/26chiraWeb/26chiraWeb-jumbo.jpg?quality=75&auto=webp',
+    'https://s.abcnews.com/images/General/pelosi-office-breach-arrest-richard-barnett-02-sh-llr-210107_1610132367804_hpMain_16x9_992.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -48,26 +56,131 @@ class _MyAppState extends State<MyApp> {
               Icons.notification_add,
               color: Colors.black,
             ),
-            onPressed: () {},
+            onPressed: () {
+              print('presionado');
+            },
           ),
         ],
       ),
-      body: Column(
-        children: [
-          SizedBox(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
               width: double.infinity,
               height: 40.0,
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
+                  const Text(
                     'Updated: ',
                     style: TextStyle(color: Colors.black38),
                   ),
+                  Text(
+                    DateFormat('yMMMMd').format(DateTime.now()),
+                  ),
+                  const Text(
+                    ' at ',
+                    style: TextStyle(color: Colors.black38),
+                  ),
+                  Text(
+                    DateFormat('jm').format(DateTime.now()),
+                  ),
                 ],
-              )),
-        ],
+              ),
+            ),
+            CarouselSlider.builder(
+              options: CarouselOptions(
+                height: 200,
+                onPageChanged: (index, reason) =>
+                    setState((() => activeIndex = index)),
+              ),
+              itemCount: urlImages.length,
+              itemBuilder: (context, index, realIndex) {
+                final urlImage = urlImages[index];
+
+                return buildImage(urlImage, index);
+              },
+            ),
+            const SizedBox(height: 10),
+            buildIndicator(),
+            Container(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Title(
+                    color: Colors.black,
+                    child: const Text(
+                      'Pelosi Wants to Win House, but Can she Corral The Democrats?',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 25.0),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  ListTile(
+                    leading: MyBullet(),
+                    title: const Text(
+                      'At 77, Representative Nancy Pelosi remains a dominant, but polarizing, figure in Washington.',
+                    ),
+                  ),
+                  ListTile(
+                    leading: MyBullet(),
+                    title: const Text(
+                      "How she bridges Democrats' factions on inmigration may help determine whther she can lead her party to a majority",
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Text('2h ago'),
+                    trailing: Row(
+                      children: [],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     ));
+  }
+
+  Widget buildImage(String urlImage, int index) => Container(
+        margin: const EdgeInsets.symmetric(horizontal: 12),
+        color: Colors.grey,
+        child: Image.network(
+          urlImage,
+          fit: BoxFit.cover,
+        ),
+      );
+
+  Widget buildIndicator() => AnimatedSmoothIndicator(
+        activeIndex: activeIndex,
+        count: urlImages.length,
+        effect: const SlideEffect(
+            spacing: 8.0,
+            radius: 4.0,
+            dotWidth: 24.0,
+            dotHeight: 8.0,
+            paintStyle: PaintingStyle.stroke,
+            strokeWidth: 1.5,
+            dotColor: Colors.grey,
+            activeDotColor: Colors.indigo),
+      );
+}
+
+class MyBullet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 5.0,
+      width: 5.0,
+      decoration: const BoxDecoration(
+        color: Colors.black,
+        shape: BoxShape.circle,
+      ),
+    );
   }
 }
